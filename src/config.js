00f5a6ce7127bbcd8f23c50b356d7928e6c87eb1
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-11-13 13:58:45 -0800

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "new-notes-app-client-marlon-castaneda"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://zg8ciqp35l.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_4RIhoNzrA",
+      APP_CLIENT_ID: "7b34stnrmi8kpek8ilqecafnus",
+      IDENTITY_POOL_ID: "us-east-1:fa7ff4ab-7c52-4014-b9b4-d7fd1f6d5e0d"
+    }
+  };
